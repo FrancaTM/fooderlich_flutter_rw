e72f0d0d9e345957ed24bada6cf5e29199f7cb14
FrancaTM <francatm@outlook.com>
2022-03-03 18:42:46 -0300

    add home widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,23 @@
 import 'package:flutter/material.dart';
 
 import 'fooderlich_theme.dart';
+import 'home.dart';
 
 void main() {
-  // 1
   runApp(const Fooderlich());
 }
 
 class Fooderlich extends StatelessWidget {
-  // 2
   const Fooderlich({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final theme = FooderlichTheme.dark();
     // TODO: Apply Home widget
-    // 3
     return MaterialApp(
       theme: theme,
       title: 'Fooderlich',
-      // 4
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Fooderlich', style: theme.textTheme.headline6),
-        ),
-        body: Center(
-          child: Text(
-            'Let\'s get cooking 👩‍🍳',
-            style: theme.textTheme.headline1,
-          ),
-        ),
-      ),
+      home: const Home(),
     );
   }
 }
