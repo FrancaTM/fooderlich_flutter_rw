27428a6b49afb3832ca0d2662b6d13307301ed94
FrancaTM <francatm@outlook.com>
2022-03-04 15:31:35 -0300

    add image

--- lib/card1.dart
+++ lib/card1.dart
@@ -11,8 +11,21 @@ class Card1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      // TODO: Card1 Decorate Container
-      child: Container(),
+      child: Container(
+        // TODO: Add a stack of text
+        padding: const EdgeInsets.all(16),
+        constraints: const BoxConstraints.expand(
+          width: 350,
+          height: 450,
+        ),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/mag1.png'),
+            fit: BoxFit.cover,
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+        ),
+      ),
     );
   }
 }
